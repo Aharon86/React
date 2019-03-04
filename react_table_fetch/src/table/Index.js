cacac885335e4b056679efc29b607176ca64d9e8
Aharon86 <aharonhg@gmail.com>
2019-03-04 15:14:29 +0400

    finish react_table_fetch

--- react_table_fetch/src/table/Index.js
+++ react_table_fetch/src/table/Index.js
@@ -2,31 +2,39 @@ import React, { Component } from 'react';
 import CreateRow from './CreateRow';
 import arrData from './arrData';
 import Header from './Header';
-import Footer from './Footer';
+// import Footer from './Footer';
 import './css/table.css';
 
 
 
 class Project extends Component{
     state = {
-        data: []
+        data: [],
+        busy: true,
+        available: true,
+        started: true
     }
     componentDidMount = () => {
         this.sendData();
-      }
+    }
+    reload = () => {
+        this.setState({data: []});
+        this.sendData();
+    }
 
     sendData = () => {
-       let data = arrData();
-       return fetch("http://rest.learncode.academy/api/johnbob/friends", {
-            method: 'POST', 
-            body: JSON.stringify(data),
-            headers:{
-            'Content-Type': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then((response) => {this.setState({data: response})})
-        .catch(error => console.error('Error:', error));
+        let data = arrData();
+        console.log(22);
+        fetch("http://rest.learncode.academy/api/johnbob/friends", {
+                method: 'POST', 
+                body: JSON.stringify(data),
+                headers:{
+                'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then((response) => {this.setState({data: response})})
+            .catch(error => console.error('Error:', error));
 
     }
 
@@ -34,11 +42,22 @@ class Project extends Component{
     showRow = () => {
         let beackData = this.state.data;
         let arrComp = [];
+        console.log(this.state.busy);
+        console.log(beackData);
         if (beackData.length === 0) {
-           arrComp.push(<CreateRow row_table = "Load..." key={1} />);
+           arrComp.push(<CreateRow row_table = 'Load...' key={1} />);
         } else{
             for (let i = 0; i < beackData.length; i++) {
-                arrComp.push(<CreateRow row_table = {beackData[i]} key = {i} />);
+                if (beackData[i].status === "Busy" && this.state.busy) {
+                    arrComp.push(<CreateRow row_table = {beackData[i]} key = {i} />);
+                } 
+                if (beackData[i].status === "Available" && this.state.available) {
+                    arrComp.push(<CreateRow row_table = {beackData[i]} key = {i} />);
+                } 
+                if (beackData[i].status === "Started" && this.state.started) {
+                    arrComp.push(<CreateRow row_table = {beackData[i]} key = {i} />);
+                } 
+              
                 
             }
             
@@ -48,13 +67,17 @@ class Project extends Component{
     }
 
     render() {
-        
         return (
             <table>
                 <Header />
                 <tbody>
                     {this.showRow()}
-                    <Footer />
+                    <tr>
+                        <td><input type='checkbox' id='busy' onChange = {() => {this.setState({busy: !this.state.busy})}} checked={this.state.busy} /> Busy</td>
+                        <td><input type='checkbox' id='available' onChange = {() => {this.setState({available: !this.state.available})}} checked={this.state.available} /> Available</td>
+                        <td><input type='checkbox' id='started' onChange = {() => {this.setState({started: !this.state.started})}} checked={this.state.started} /> Started</td>
+                        <td><button type='button' onClick = {this.reload}>Reload</button></td>
+                    </tr>
                 </tbody>
             </table>
         );
