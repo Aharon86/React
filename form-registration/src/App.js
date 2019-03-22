ef194cb371b215f3ee3b333174adb462da1ee90f
Aharon86 <aharonhg@gmail.com>
2019-03-22 14:31:13 +0400

    add form-scss

---
+++ form-registration/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Form from './form/Home';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+     <Form />
+    );
+  }
+}
+
+export default App;
